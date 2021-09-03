38717754c5693ece51221a5f453c398dbb4748a7
Jacob z <24jzweiback@athenian.org>
2021-09-03 10:04:18 -0700

    first attempt

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -8,35 +8,51 @@ public class MyMain {
     // Given three ints x, y, and z, this method
     // returns the largest of the three numbers
     public static int findBiggestNumber(int x, int y, int z) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        if (x>y && x>z) {
+            return(x);
+        }
+        else if (y>x && y>z) {
+            return(y);
+        }
+        else {
+            return(z);
+        }
     }
 
     // This method simulates the flip of a coin, where
     // true represents "heads" and false represents "tails"
     // This method should return true half the time, and false the other half
     public static boolean flipCoin() {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        return (Math.random()>=0.5);
+        /*double rand_val=Math.random();
+        if (rand_val>=0.5){
+            return true;
+        }
+        else{
+            return false;
+        }*/
     }
 
     // This method simulates the flip of a weighted coin,
     // that is a coin that is biased towards heads
     public static boolean flipWeightedCoin() {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        double rand_val=Math.random();
+        if (rand_val>0.7){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     // This method rounds a double to the nearest whole number
     public static int round(double d) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        //(int((Math.random()*6)+1));
     }
 
     // This method returns a random number between 1 and 6, inclusive
     public static int rollDie() {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+
     }
 
     // This method carries out the quadratic formula and *prints* out the answers
@@ -46,10 +62,13 @@ public class MyMain {
 
     public static void main(String[] args) {
         // Test the add5 method
-        int x = 2;
-        int y = add5(x);
-        System.out.println(y);
+        /*int x = 2;
+        int y = 3;
+        int z = 5;
+        int big=(findBiggestNumber(x,y,z));
+        System.out.println(big);*/
 
-        // YOUR CODE HERE
+        //System.out.println(flipCoin());
+        System.out.println(flipWeightedCoin());
     }
 }
